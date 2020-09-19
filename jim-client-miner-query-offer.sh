5ff4867c2a471cb22d0173f5f3d04ef188f38421
Jim Pick <jim@jimpick.com>
2020-09-19 14:08:01 -0700

    Add helpful dev scripts

---
+++ jim-client-miner-query-offer.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+curl -X POST  -H "Content-Type: application/json"  -H "Authorization: Bearer $(cat ~/.lotus/token)"  --data '{ "jsonrpc": "2.0", "method": "Filecoin.ClientMinerQueryOffer", "params": ["t01000", { "/": "bafk2bzaceadshj2ri6umifoo4bhwvzhqe3fe22hgslw76ovro4pre22zjmcbi" }, null], "id": 1 }'  'http://127.0.0.1:1234/rpc/v0' | jq -C . | less -RM
---
+++ jim-test-retrieval.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+set -x
+
+time lotus client retrieve bafk2bzaceadshj2ri6umifoo4bhwvzhqe3fe22hgslw76ovro4pre22zjmcbi /home/ubuntu/downloads/output-$(date +'%s').txt
