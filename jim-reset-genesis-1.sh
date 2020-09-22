502a9baacbaeb2147128a62dca3f708991dc45c6
Jim Pick <jim@jimpick.com>
2020-09-22 12:52:01 -0700

    More cleanup for genesis

--- jim-reset-genesis-1.sh
+++ jim-reset-genesis-1.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 killall lotus
-rm -rf ~/.lotus ~/.lotusminer ~/.genesis-sectors ~/tmp/*.log
+rm -rf ~/.lotus ~/.lotusminer ~/.genesis-sectors ~/tmp/*.log localnet.json *.car
 ./lotus-seed pre-seal --sector-size 2KiB --num-sectors 2
 ./lotus-seed genesis new localnet.json
 ./lotus-seed genesis add-miner localnet.json ~/.genesis-sectors/pre-seal-t01000.json
