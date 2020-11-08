a04410cce51617c7ea7c9a5d9c93cab1dea6659f
Jim Pick <jim@jimpick.com>
2020-11-08 10:54:32 -0800

    Can successfully call remote JSON-RPC API from Lotus WASM

--- node/impl/client/client_query_ask.go
+++ node/impl/client/client_query_ask.go
@@ -2,25 +2,32 @@ package client
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/markets/utils"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"golang.org/x/xerrors"
+	// "github.com/filecoin-project/lotus/chain/types"
+	// "github.com/filecoin-project/lotus/markets/utils"
 )
 
 func (a *API) ClientQueryAsk(ctx context.Context, p peer.ID, miner address.Address) (*storagemarket.StorageAsk, error) {
+	fmt.Printf("Jim node impl ClientQueryAsk %v %v\n", p, miner)
 	mi, err := a.StateMinerInfo(ctx, miner, types.EmptyTSK)
 	if err != nil {
 		return nil, xerrors.Errorf("failed getting miner info: %w", err)
 	}
+	fmt.Printf("Jim node impl ClientQueryAsk minerinfo %v\n", mi)
+	return nil, xerrors.Errorf("jim1 - clientqueryask")
 
-	info := utils.NewStorageProviderInfo(miner, mi.Worker, mi.SectorSize, p, mi.Multiaddrs)
-	ask, err := a.SMDealClient.GetAsk(ctx, info)
-	if err != nil {
-		return nil, err
-	}
-	return ask, nil
+	/*
+		info := utils.NewStorageProviderInfo(miner, mi.Worker, mi.SectorSize, p, mi.Multiaddrs)
+		ask, err := a.SMDealClient.GetAsk(ctx, info)
+		if err != nil {
+			return nil, err
+		}
+		return ask, nil
+	*/
 }
