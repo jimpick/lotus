05b0885e7bdbc50e6914b515eea136123465e572
Jim Pick <jim@jimpick.com>
2020-12-03 17:57:32 -0800

    Add p2pclient for use with go-fil-markets branch

--- node/modules/clientstorage_queryask.go
+++ node/modules/clientstorage_queryask.go
@@ -7,10 +7,11 @@ import (
 	storageimpl "github.com/filecoin-project/go-fil-markets/storagemarket/impl"
 	smnet "github.com/filecoin-project/go-fil-markets/storagemarket/network"
 	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-daemon/p2pclient"
 )
 
-func StorageClient(lc fx.Lifecycle, h host.Host, scn storagemarket.StorageClientNode) (storagemarket.StorageClient, error) {
-	net := smnet.NewFromLibp2pHost(h)
+func StorageClient(lc fx.Lifecycle, h host.Host, p2pclientNode *p2pclient.Client, scn storagemarket.StorageClientNode) (storagemarket.StorageClient, error) {
+	net := smnet.NewFromLibp2pHost(h, p2pclientNode)
 	c, err := storageimpl.NewClient(net, scn)
 	if err != nil {
 		return nil, err
